4516e57d9f4c8304dbaab22308521d68879616de
Mandresy <christianmandresy@gmail.com>
2022-01-10 09:43:33 +0300

    Mampiasa annotation amle validator de annotation amle hibernate

--- src/main/java/com/cloudproject/backoffice/model/Region.java
+++ src/main/java/com/cloudproject/backoffice/model/Region.java
@@ -1,8 +1,19 @@
 package com.cloudproject.backoffice.model;
 
+import javax.persistence.*;
+
+
+
+@Entity
+@Table(name="Region")
 public class Region {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "IdRegion")
     private int IdRegion;
+
+    @Column(name = "NomRegion")
     private String NomRegion;
 
     public Region() {
