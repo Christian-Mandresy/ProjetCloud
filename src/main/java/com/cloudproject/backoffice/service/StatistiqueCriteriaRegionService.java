bc38fe8e26b17fef01eb4d8628446fe6df361a78
Mandresy <christianmandresy@gmail.com>
2022-01-12 02:16:52 +0300

    Service anle entite rehetra ilaina @statistique

---
+++ src/main/java/com/cloudproject/backoffice/service/StatistiqueCriteriaRegionService.java
@@ -0,0 +1,8 @@
+package com.cloudproject.backoffice.service;
+
+import java.util.Date;
+import java.util.List;
+
+public interface StatistiqueCriteriaRegionService {
+    List getStat(int type, int etat, String region, Date dateDeb, Date dateFin);
+}
